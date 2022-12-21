6e389d33b05dcfb8778e28df439c24ede0064cc2
TomekGrzybek <t.grzybowski94@gmail.com>
2022-12-21 19:11:22 +0100

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import _, { now } from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+});
+
+function onTimeUpdate(timeupdate) {
+  localStorage.setItem('videoplayer-current-time', timeupdate.seconds);
+}
+
+player.on('timeupdate', _.throttle(onTimeUpdate, 1000));
