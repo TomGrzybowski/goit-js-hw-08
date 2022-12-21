ec34ec7857f3f1a6e493d19d8634aca2b6b3f24e
TomekGrzybek <t.grzybowski94@gmail.com>
2022-12-21 15:40:02 +0100

    Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+
+//Create galery by inserting images and links
+
 console.log(galleryItems);
+
+//Create galery by inserting images and links
+galleryItems.forEach(el => {
+  const link = document.createElement('a');
+  link.classList.add('gallery__item');
+  link.href = el.original;
+
+  const img = document.createElement('img');
+  img.classList.add('gallery__image');
+  img.src = el.preview;
+  img.alt = el.description;
+
+  link.insertAdjacentElement('beforeend', img);
+  gallery.insertAdjacentElement('beforeend', link);
+});
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
